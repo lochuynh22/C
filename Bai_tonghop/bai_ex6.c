1578b93c73b1269c58100b1c5509a90189949cdb
lochuynh22 <huynhlatienloc123@gmail.com>
2024-12-13 22:52:36 +0700

    tonghop

---
+++ Bai_tonghop/bai_ex6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+
+int main(){
+    
+    int num, dem=0;
+    printf("Nhap 1 so nguyen duong:");
+    scanf("%d", &num);
+
+    return 0;
+}
--- ss_3/bt_8.c
+++ ss_3/bt_8.c
@@ -4,7 +4,13 @@ int main(){
     int num, reverse_num=0;
     printf("Nhap mot so nguyen co 4 chu so:");
     scanf("%d", &num);
-    reverse_num += (num%10)*1000+(num % 100)*100+(num%1000)*10;
+    reverse_num += (num%10)*1000;
+    num/=10;
+    reverse_num += (num%10)*100;
+    num/=10;
+    reverse_num += (num%10)*10;
+    num/=10;
+    reverse_num+=num;
     printf("So nghich dao la:%d", reverse_num);
 
     return 0;
