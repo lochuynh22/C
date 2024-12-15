ac54250be5b49c6cb66d9d5de0b92f55f6b31b9e
lochuynh22 <huynhlatienloc123@gmail.com>
2024-12-15 13:51:23 +0700

    BT_tonghop

--- Bai_tonghop/bai_ex6.c
+++ Bai_tonghop/bai_ex6.c
@@ -1,11 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 
+int kiemtra(int n) {
+    if (n < 2) return 0;
+    int kt = 0;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            kt = 1;
+            break;
+        }
+    }
+    return kt;
+}
 
-int main(){
-    
-    int num, dem=0;
-    printf("Nhap 1 so nguyen duong:");
-    scanf("%d", &num);
+int main() {
+    int n;
+    printf("nhap so nguyen duong n: ");
+    scanf("%d", &n);
 
+    printf("%d so nguyen to dau tien la: ", n);
+    int dem = 0;
+    for (int i = 2; dem < n; i++) {
+        if (kiemtra(i) == 0) {
+            printf("%d ", i);
+            dem++;
+        }
+    }
     return 0;
 }
